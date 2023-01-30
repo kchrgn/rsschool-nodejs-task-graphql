cb406cd8fa56888508c61784d99743f0bece0c0f
kchrgn <kchrgn@gmail.com>
2023-01-30 23:48:20 +0300

    feat: implement gql requests in accordance task 2.3

--- src/routes/graphql/userType.ts
+++ src/routes/graphql/userType.ts
@@ -1,4 +1,7 @@
 import { GraphQLObjectType, GraphQLString, GraphQLNonNull, GraphQLList } from 'graphql';
+import { postType } from './postType';
+import { profileType } from './profileType';
+import { memberType } from './memberType';
 
 export const userType = new GraphQLObjectType({
   name: 'User',
@@ -8,5 +11,24 @@ export const userType = new GraphQLObjectType({
     lastName:             { type: GraphQLString },
     email:                { type: GraphQLString },  
     subscribedToUserIds:  { type: new GraphQLList(GraphQLString)},
+    posts: {
+      type: new GraphQLList(postType),
+      async resolve(source, args, context) {
+        return await context.db.posts.findMany({key: 'userId', equals: source.id});
+      },
+    },
+    profile: {
+      type: profileType,
+      async resolve(source, args, context) {
+        return await context.db.profiles.findOne({key: 'userId', equals: source.id});
+      },
+    },
+    member: {
+      type: memberType,
+      async resolve(source, args, context) {
+        const profile = await context.db.profiles.findOne({key: 'userId', equals: source.id});
+        return await context.db.memberTypes.findOne({key: 'id', equals: profile?.memberTypeId});
+      },
+    }
   }),
 })
